6881f24722dca7b4d707b089188594dbb94aeb27
g-1704 <77138518+g-1704@users.noreply.github.com>
2022-12-28 03:56:57 -0500

    logged input to the console

--- client/components/CreateNew.jsx
+++ client/components/CreateNew.jsx
@@ -1,29 +1,37 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useState, useRef } from 'react';
 import uuid from "react-uuid";
 import './CreateNew.Style.css';
 
 function CreateNew () {
-
+    const bodyRef = useRef();
+    const titleRef = useRef();
+    const [newNote, setNewNote] = useState({});
     const [notes, setNotes] = useState([]);
 
     const onAddNote = () => {
      console.log('hi');
-      const newNote = {
+      const newNoteTemplete = {
        id: uuid(),
-       title: " ",
-       body:" ",
+       title: titleRef.current.value,
+       body:bodyRef.current.value,
        date : Date.now(),
       };
+      console.log(bodyRef.current.value);
+      console.log(titleRef.current.value);
+      console.table(newNoteTemplete);
+      const note = Object.assign(newNoteTemplete, {title: bodyRef.current.value, title: titleRef.current.value});
+      setNewNote(newNote);
       setNotes([newNote, ...notes]);
+      console.log( newNote );
     };
 
   return (
   <div className="app-createNew">
   <div className="app-createNew-write">
   <div className = "body"> 
-     <input className="input-title" type ="text" id="title" placeholder="Title here" /> 
+     <input className="input-title" type ="text" id="title" placeholder="Title here" ref={titleRef} /> 
      <div className="textarea">
-         <input className= "textbox" id="body" placeholder="Start here"/> 
+         <input className= "textbox" id="body" placeholder="Start here" ref={bodyRef}/> 
      
       <button onClick={onAddNote}>Save</button>
       </div>
