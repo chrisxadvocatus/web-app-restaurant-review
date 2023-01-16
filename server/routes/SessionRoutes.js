da41058880d76dd5e0358a444eb2f9bec6ca306f
Kevin Grimaldi <anthgrim@icloud.com>
2023-01-16 11:53:29 -0500

    fixing server.js imports

--- server/routes/SessionRoutes.js
+++ server/routes/SessionRoutes.js
@@ -1,15 +1,14 @@
 import express from 'express'
 import { start, update, clearUp } from '../controllers/SessionControllers.js'
-import session from '../middlewares/session.js'
 import cookie from '../middlewares/cookie.js'
 
 // Initialize router
 const router = express.Router()
 
-router.get('/', cookie.setCookie, session.startSession, start)
+router.get('/', cookie.setCookie, start)
 
-router.post('/update', session.updateSession, update)
+router.post('/update', update)
 
-router.delete('/clearUp', session.clearupSession, clearUp)
+router.delete('/clearUp', clearUp)
 
 export default router
