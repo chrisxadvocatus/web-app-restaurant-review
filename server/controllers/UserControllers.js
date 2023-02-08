d34359a20119ad35ab2176ca88b615d5d8068dad
PetrosO123 <pmo20@miami.edu>
2023-02-08 12:53:03 -0500

    added user authentication related error messages

--- server/controllers/UserControllers.js
+++ server/controllers/UserControllers.js
@@ -2,17 +2,51 @@ import User from '../models/UserModel.js'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
 
-/**
- * @description Sign Up user
- */
-export const signUp = async (req, res) => {}
 
 /**
- * @description Sing In user
- */
+* @description Sign Up user
+*/
+export const signUp = async (req, res) => {
+   const {name, email, password} = req.body
+
+
+   if (!name || !email || !password){
+       //if no name, no email, or no password
+       return res.status(400).json({
+           message: 'Missing 1 or more required fields.'
+       })
+   }
+   try{
+       //validate if email already exists, and user is signed up
+       //check if email is already registered
+       const isDuplicate = await User.findOne({email}).exec()
+
+
+       if (isDuplicate){
+           return res.status(409).json({
+               message:'Email is already registered'
+           })
+       }
+   }catch (error){
+       console.log(error)
+       return res.status(500).json({
+           message: 'SERVER ERROR',
+           error
+       })
+   }
+}
+
+
+/**
+* @description Sing In user
+*/
 export const signIn = async (req, res) => {}
 
+
 /**
- * @description Sign Out user
- */
+* @description Sign Out user
+*/
 export const signOut = async (req, res) => {}
+
+
+
